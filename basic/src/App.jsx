3da1b8489620ce87d956196e6c2bd87ae4d65162
igaozp <igaozp@hotmail.com>
2021-03-14 16:30:51 +0800

    Refactor: remove unused code

--- basic/src/App.jsx
+++ basic/src/App.jsx
@@ -10,45 +10,30 @@ const Hello = (props) => {
     )
 }
 
-function App() {
+const Header = () => {
     const [count, setCount] = useState(0)
-
     const now = '2021-03-14'
+
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>Hello Vite + React!</p>
+            <p>
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is: {count}
+                </button>
+            </p>
+            <Hello name="world" time={'2021-03-14'}/>
+            <Hello name="React" time={now}/>
+        </header>
+    )
+}
+
+function App() {
+
     return (
         <>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>Hello Vite + React!</p>
-                <p>
-                    <button onClick={() => setCount((count) => count + 1)}>
-                        count is: {count}
-                    </button>
-                </p>
-                <Hello name="world" time={'2021-03-14'}/>
-                <Hello name="React" time={now}/>
-                <p>
-                    Edit <code>App.jsx</code> and save to test HMR updates.
-                </p>
-                <p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    {' | '}
-                    <a
-                        className="App-link"
-                        href="https://vitejs.dev/guide/features.html"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Vite Docs
-                    </a>
-                </p>
-            </header>
+            <Header/>
         </>
     )
 }
