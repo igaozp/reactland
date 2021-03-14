6ff3ced63c220efb3a235c4605a9d1ab7c90f816
igaozp <igaozp@hotmail.com>
2021-03-14 16:41:07 +0800

    Feat: course exercise demo

--- basic/src/App.jsx
+++ basic/src/App.jsx
@@ -10,9 +10,29 @@ const Hello = (props) => {
     )
 }
 
+const Course = (props) => {
+    return (
+        <h1>{props.name}</h1>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <p>{props.part} {props.exercise}</p>
+    )
+}
+
 const Header = () => {
     const [count, setCount] = useState(0)
+
     const now = '2021-03-14'
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercise1 = 10
+    const part2 = 'Using props to pass data'
+    const exercise2 = 7
+    const part3 = 'State of a component'
+    const exercise3 = 14
 
     return (
         <header className="App-header">
@@ -25,6 +45,11 @@ const Header = () => {
             </p>
             <Hello name="world" time={'2021-03-14'}/>
             <Hello name="React" time={now}/>
+
+            <Course name={course}/>
+            <Content part={part1} exercise={exercise1}/>
+            <Content part={part2} exercise={exercise2}/>
+            <Content part={part3} exercise={exercise3}/>
         </header>
     )
 }
