6d7d92f427ea26a3c8fc1d47c1be29b483cd3333
igaozp <igaozp@hotmail.com>
2021-03-17 20:49:05 +0800

    Feat: unicafe demo first version

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,14 +1,28 @@
 import React, {useState} from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
+    const [good, setGoodCount] = useState(0)
+    const [neutral, setNeutralCount] = useState(0)
+    const [bad, setBadCount] = useState(0)
+
+    const goodClick = () => setGoodCount(good + 1)
+    const neutralClick = () => setNeutralCount(neutral + 1)
+    const badClick = () => setBadCount(bad + 1)
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-            </header>
-        </div>
+        <>
+            <h1>give feedback</h1>
+
+            <button onClick={goodClick}>good</button>
+            <button onClick={neutralClick}>neutral</button>
+            <button onClick={badClick}>bad</button>
+
+            <h1>statistics</h1>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+        </>
     )
 }
 
