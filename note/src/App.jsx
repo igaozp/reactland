9c29ebaf8ec9af4fc87067f09761fb175b94e2d0
igaozp <igaozp@hotmail.com>
2021-03-21 18:43:23 +0800

    Refactor: improvement note list display

--- note/src/App.jsx
+++ note/src/App.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import './App.css'
 
-const App = (props) => {
-  const { notes } = props
-  return (
-      <div>
-        <h1>Notes</h1>
-        <ul>
-          <li>{notes[0].content}</li>
-          <li>{notes[1].content}</li>
-          <li>{notes[2].content}</li>
-        </ul>
-      </div>
-  )
+const App = ({notes}) => {
+    return (
+        <div>
+            <h1>Notes</h1>
+            <ul>
+                {notes.map(note => <li key={note.id}>{note.content}</li>)}
+            </ul>
+        </div>
+    )
 }
 
 export default App
