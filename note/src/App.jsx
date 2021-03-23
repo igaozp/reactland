b74137ec9805c3f50d60b4d6542c7c601fe94580
igaozp <igaozp@hotmail.com>
2021-03-23 20:48:21 +0800

    Feat: show all button

--- note/src/App.jsx
+++ note/src/App.jsx
@@ -5,6 +5,7 @@ import Note from "./components/Note";
 const App = (props) => {
     const [notes, setNotes] = useState(props.notes)
     const [newNote, setNewNote] = useState('a new note...')
+    const [showAll, setShowAll] = useState(true)
 
     const addNote = (event) => {
         event.preventDefault()
@@ -24,11 +25,16 @@ const App = (props) => {
         setNewNote(event.target.value)
     }
 
+    const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+
     return (
         <div>
             <h1>Notes</h1>
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>show {showAll ? 'important' : 'all'}</button>
+            </div>
             <ul>
-                {notes.map(note => <Note key={note.id} note={note}/>)}
+                {notesToShow.map(note => <Note key={note.id} note={note}/>)}
             </ul>
             <form onSubmit={addNote}>
                 <input value={newNote} onChange={handleNoteChange}/>
